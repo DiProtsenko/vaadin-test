a229bb850f4bcf5e2c273e85283b4ae0013c77bb
Dmitry Protsenko <43979902+DiProtsenko@users.noreply.github.com>
2019-12-19 09:36:29 +0500

    Update Contact.java

--- src/main/java/dev/protsenko/vaadin/entities/Contact.java
+++ src/main/java/dev/protsenko/vaadin/entities/Contact.java
@@ -2,7 +2,7 @@ package dev.protsenko.vaadin.entities;
 
 import lombok.Getter;
 import lombok.Setter;
-
+import lombok.EqualsAndHashCode;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
